3bb30f08f1e93901f417a7837c472abbf97bd6ea
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-12 16:09:11 -0400

    Installed bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>about <br/> coffee</h1>
+      <div className="container">
+        <div className="col-6">
+          <h1>about <br/> coffee</h1>
+        </div>
+        <div className="col-6">
+        </div>
       </div>
     );
   }
