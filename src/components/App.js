9773dba8accdb5b502300035021a85ce9ada68e7
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-12 16:23:14 -0400

    Added coffee placeholder, and more title styling.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.scss';
 
+import coffeeLanding from '../assets/coffee.svg';
+
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="col-6">
-          <h1>about <br/> coffee</h1>
-        </div>
-        <div className="col-6">
+      <div className="container" id="landing">
+        <div className="row">
+          <div className="col-12 col-sm-6">
+            <h1 className="title">about <br/> coffee</h1>
+          </div>
+          <div className="col-12 col-sm-6">
+            <img style={{width: '100%'}} src={coffeeLanding}/>
+          </div>
         </div>
       </div>
     );
