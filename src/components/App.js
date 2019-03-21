d02f86f1679a861a59e81317bd577f7aed7af990
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-21 16:36:38 -0400

    Refactored landing contents to separate component, put routes in App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/styles.scss';
 
-import coffeeLanding from '../assets/coffee.svg';
 
 class App extends React.Component {
   render() {
     return (
-        <div className="container" id="landing">
-          <div className="row">
-            <div className="col-12 col-sm-6 col-md-7">
-              <img className="coffee-image" src={coffeeLanding}/>
-            </div>
-            <div className="col-12 col-sm-6 col-md-5 coffee-greeting">
-              <div class="content">
-                <h1 className="title">about<br/>coffee</h1>
-                <button className="btn btn-big">
-                  ENTER
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+      <BrowserRouter>
+        <Route path="/" exact component={Landing}/>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import coffeeLanding from '../assets/coffee.svg';
+
+class Landing extends React.Component {
+  render() {
+    return (
+      <div className="container" id="landing">
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-7">
+            <img className="coffee-image" src={coffeeLanding}/>
+          </div>
+          <div className="col-12 col-sm-6 col-md-5 coffee-greeting">
+            <div class="content">
+              <h1 className="title">about<br/>coffee</h1>
+              <button className="btn btn-big">
+                ENTER
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
