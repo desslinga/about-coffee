759a72acabfa070524b2b0d236dbdde60171c772
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-21 14:09:22 -0400

    Centering landing page in progress

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,18 @@ import coffeeLanding from '../assets/coffee.svg';
 class App extends React.Component {
   render() {
     return (
-      <div className="container" id="landing">
-        <div className="row">
-          <div className="col-12 col-sm-6 align-center">
-            <h1 className="title">about <br/> coffee</h1>
-            <button className="btn btn-big">
-              ENTER
-            </button>
-          </div>
-          <div className="col-12 col-sm-6">
-            <img style={{width: '100%'}} src={coffeeLanding}/>
+      <div className="align-center">
+        <div className="container" id="landing">
+          <div className="row">
+            <div className="col-12 col-sm-6">
+              <img className="coffee-image" src={coffeeLanding}/>
+            </div>
+            <div className="col-12 col-sm-6 align-center">
+              <h1 className="title">about <br/> coffee</h1>
+              <button className="btn btn-big">
+                ENTER
+              </button>
+            </div>
           </div>
         </div>
       </div>
