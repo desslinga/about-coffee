ee65390c841737d9c843fcfa3dab2a2e570380a4
Fides Linga <fides.linga@mail.utoronto.ca>
2019-03-21 15:54:49 -0400

    More styling and position changes.

--- src/components/App.js
+++ src/components/App.js
@@ -7,21 +7,21 @@ import coffeeLanding from '../assets/coffee.svg';
 class App extends React.Component {
   render() {
     return (
-      <div className="align-center">
         <div className="container" id="landing">
           <div className="row">
-            <div className="col-12 col-sm-6">
+            <div className="col-12 col-sm-6 col-md-7">
               <img className="coffee-image" src={coffeeLanding}/>
             </div>
-            <div className="col-12 col-sm-6 align-center">
-              <h1 className="title">about<br/>coffee</h1>
-              <button className="btn btn-big">
-                ENTER
-              </button>
+            <div className="col-12 col-sm-6 col-md-5 coffee-greeting">
+              <div class="content">
+                <h1 className="title">about<br/>coffee</h1>
+                <button className="btn btn-big">
+                  ENTER
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
